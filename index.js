484314147f0cf68996bd6f5232b0fd8855583c4f
JOana Falcão <joanamfalcaos@gmail.com>
2025-05-15 21:42:02 -0300

    Fix para subir no vercel

--- index.js
+++ index.js
@@ -6,12 +6,5 @@ app.get("/", (req, res) => {
   res.send("Atividade p/ disciplina aplicacoes orientadas a servicos. UNICAP.");
 });
 
-if (require.main === module) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-  });
-}
-
 module.exports = app;
 
